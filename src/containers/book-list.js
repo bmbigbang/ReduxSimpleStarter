c620b08c2f7335fc234117ef8ae6072b692cd968
bmbigbang <bm.bigbang@gmail.com>
2018-05-13 09:25:59 +0100

    adding new actions and connecting them back to the containers for changing element contents reactively

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index'
+import { bindActionCreators } from 'redux'
 
 class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li key={book.title}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                    {book.title}
+                </li>
             )
         })
     }
@@ -25,4 +31,11 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(BookList)
+
+// Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+// Promote BookList from a component to a container. Make selectBook available as props.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
