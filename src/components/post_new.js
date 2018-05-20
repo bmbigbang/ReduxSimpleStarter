b36086b32b808343a4d446e163fd1d7bb0906bfd
Ardavan Shafiee <bm.bigbang@gmail.com>
2018-05-20 17:08:27 +0100

    introducing new create post functionality including callback for page reloading to main posts page

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div className='form-group'>
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className='form-control'
                     type="text"
                     {...field.input}
                 />
+                <div className>{touched ? error : ''}</div>
             </div>
         );
     }
 
+    onSubmit(values) {
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label='Title'
                     name="title"
                     component={this.renderField}
                 />
                 <Field
-                    label='Tags'
-                    name="tags"
+                    label='Categories'
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -33,11 +48,35 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
+                <button className='btn btn-primary' type='submit'>Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
 }
 
+function validate(values) {
+    // values containts form contents: {'title': ..., 'categories':... etc}
+    const errors = {};
+
+    if (!values.title || values.title.length < 3) {
+        errors.title = 'Title must be at least 3 characters long.'
+    }
+
+    if (!values.categories) {
+        errors.categories = 'Category is required.'
+    }
+
+    if (!values.content) {
+        errors.content = 'Content is required.'
+    }
+    // if errors is empty, form may submit
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
